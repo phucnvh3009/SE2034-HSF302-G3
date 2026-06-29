4f897cb283c59dac827c761b8903d2cd09e0e160
Nguyen Vu Hoang Phuc <cohphuc1234509876@gmail.com>
2026-06-29 08:35:47 +0700

    Hello

---
+++ src/test/java/vn/edu/fpt/Se2034Hsf302G3ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.fpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se2034Hsf302G3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
